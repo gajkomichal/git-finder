d23e2e638730f85aeffe1667a7f3e4c196b9d8ac
Michał Gajko <mgajk@softserveinc.com>
2020-08-21 10:44:57 +0200

    api url changes, alert styling

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const Alert = ({ alert }) => {
-  return alert !== null && <div className='alert'>{alert.msg}</div>;
+  return (
+    alert !== null && (
+      <div className='alert' style={alertStyle}>
+        {alert.msg}
+      </div>
+    )
+  );
+};
+
+const alertStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignitems: 'center',
+  margin: '0 auto',
+  width: '600px',
+  height: '50px',
+  backgroundColor: '#000',
+  borderRadius: '0px',
+  textAlign: 'center',
+  color: '#fff',
 };
 
 export default Alert;
